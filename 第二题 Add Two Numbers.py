55edcda8adb2ceeec88ddd7808aaab5984fa1db4
Samanthe-H <60735890+Samanthe-H@users.noreply.github.com>
2021-03-08 08:48:23 +0800

    Update 第二题 Add Two Numbers.py

--- 第二题 Add Two Numbers.py
+++ 第二题 Add Two Numbers.py
@@ -46,4 +46,29 @@ class Solution:
                 break
         return l3
 
-# 优化1：
+# 优化1：添加预指针
+class Solution:
+    def addTwoNumbers(self, l1, l2) :
+        '''
+        type l1: ListNode
+        type l2: ListNode
+        rtype: ListNode
+        '''
+        re = ListNode(0) # re即为预指针
+        r = re
+        v = 0
+        while(l1 or l2):
+            if l1:  v1 = l1.val
+            else:  v1 = 0
+            if l2:  v2 = l2.val
+            else:  v2 = 0
+            sum = v1 + v2 + v
+            v = sum//10
+            r.next = ListNode(sum%10)
+            r = r.next
+            if(l1!=None): l1 = l1.next
+            if(l2!=None): l2 = l2.next
+        if(v>0):
+            r.next = ListNode(1)
+        return re.next # 预指针指向0，不用输出，从下一个节点开始输出
+# 评分差不多
